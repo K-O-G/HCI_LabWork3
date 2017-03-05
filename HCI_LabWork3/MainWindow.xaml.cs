0589ec97ab792cb498a901b046b6edd7dd8187fc
K-O-G <KaterinaO.Gordiyenko@livenau.net>
2017-03-06 01:34:22 +0200

    4

--- HCI_LabWork3/MainWindow.xaml.cs
+++ HCI_LabWork3/MainWindow.xaml.cs
@@ -24,5 +24,61 @@ namespace HCI_LabWork3
         {
             InitializeComponent();
         }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExitItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AboutItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.M && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                MenuItem_Click(this, e);
+            if (e.Key == Key.E && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                ExitItem_Click(this, e);
+            if (e.Key == Key.C && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                CheckItem_Click(this, e);
+            if (e.Key == Key.A && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                AboutItem_Click(this, e);
+            MenuItem item;
+            if (e.Key == Key.C)
+            {
+                FileDialog.IsSubmenuOpen = true;
+
+            }
+            if (e.Key == Key.A && FileDialog.Items.Contains("New"))
+            {
+                FileDialog.Items.Add(new Separator());
+                FileDialog.Items.Add("New");
+                FileDialog.Items.Add("Open");
+                FileDialog.Items.Add("Close");
+                item = (MenuItem)FileDialog.Items[3];
+                FileDialog.Items.Remove("Message");
+                FileDialog.Items.Add(new Separator());
+                FileDialog.Items.Add(item);
+
+            }
+
+        }
     }
 }
